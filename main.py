7780298369bf2c541248512fe46a06c0d3118618
itsleko04 <lipnitskiyvv@gmail.com>
2025-11-22 12:58:14 +0400

    Revert "Change .ui to .py"
    This reverts commit 3846f4bbddd150bd0689fc1e593abe3eb8c765dd.

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
 import sys
 import random
 
-from UI import UI_MyWidget
-
+from PyQt6 import uic
 from PyQt6.QtCore import QPointF 
 from PyQt6.QtGui import QPainter, QColor
 from PyQt6.QtWidgets import QWidget, QApplication
 
 
-def generate_rgb():
-    r = random.randint(0, 255)
-    g = random.randint(0, 255)
-    b = random.randint(0, 255)
-    return (r, g, b)
-
-
-class MyWidget(UI_MyWidget):
+class MyWidget(QWidget):
     def __init__(self):
         super().__init__()
+        uic.loadUi(open("UI.ui", encoding="utf-8"), self)
         self.setMouseTracking(True)
-        self.setupUi(self)
+        self.initUI()
         self.pushButton.clicked.connect(lambda: self.setup_drawing_and_draw(self.draw_circle))
         self.current_draw_func = None
+
+    def initUI(self):
+        self.setGeometry(0, 0, 1000, 1000)
+        self.setWindowTitle('Рисование')
 
     def paintEvent(self, event):
         if self.current_draw_func:
